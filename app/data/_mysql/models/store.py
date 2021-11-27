8fccb585df1b59a4b8d9255315025ddde607acc6
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-11-27 03:31:20 +0100

    setup model for stores table and comment out subtypes

--- app/data/_mysql/models/store.py
+++ app/data/_mysql/models/store.py
@@ -5,37 +5,39 @@ from app.data._mysql.models import BaseModel
 
 
 class Store(BaseModel):
-    __abstract__ = True
-    # __tablename__ = "stores"
+    __tablename__ = "stores"
 
     store_id = Column(Integer, autoincrement=True, primary_key=True)
     store_type = Column(CHAR(length=1), nullable=False)
     phone = Column(String(length=25), nullable=False)
     email = Column(String(length=100), nullable=False)
 
-    # fk store_id
+    employees = relationship("Employee", back_populates="store")
+
+    # back_populates = StoreHasProduct.store
+    # products will be a list of StoreHasProduct (accessible through StoreHasProduct.product)
+    products = relationship("StoreHasProduct", back_populates="store")
 
     # UNIQUE (store_id, store_type)
 
-
-class OnlineStore(BaseModel):
-    __tablename__ = "stores"
-
-    store_id = Column(Integer, autoincrement=True, primary_key=True)
-    store_type = Column(CHAR(length=1), nullable=False)
-
-    # FOREIGN KEY (store_id, store_type)
-    #    REFERENCES stores (store_id, store_type) ON DELETE CASCADE
-
-
-class PhysicalStore(BaseModel):
-    __tablename__ = "stores"
-
-    store_id = Column(Integer, autoincrement=True, primary_key=True)
-    store_type = Column(CHAR(length=1), nullable=False)
-    address = Column(String(length=125), nullable=False)
-    zip_code = Column(String(length=7), nullable=False)
-    city = Column(String(length=50), nullable=False)
-
-    # FOREIGN KEY (store_id, store_type)
-    #    REFERENCES stores (store_id, store_type) ON DELETE CASCADE
+# class OnlineStore(BaseModel):
+#     __tablename__ = "online_stores"
+#
+#     store_id = Column(Integer, autoincrement=True, primary_key=True)
+#     store_type = Column(CHAR(length=1), nullable=False)
+#
+#     # FOREIGN KEY (store_id, store_type)
+#     #    REFERENCES stores (store_id, store_type) ON DELETE CASCADE
+#
+#
+# class PhysicalStore(BaseModel):
+#     __tablename__ = "physical_stores"
+#
+#     store_id = Column(Integer, autoincrement=True, primary_key=True)
+#     store_type = Column(CHAR(length=1), nullable=False)
+#     address = Column(String(length=125), nullable=False)
+#     zip_code = Column(String(length=7), nullable=False)
+#     city = Column(String(length=50), nullable=False)
+#
+#     # FOREIGN KEY (store_id, store_type)
+#     #    REFERENCES stores (store_id, store_type) ON DELETE CASCADE
