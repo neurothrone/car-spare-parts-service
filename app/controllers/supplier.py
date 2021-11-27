20e8f4282f31bf0769014c407a6afbb74b9336f1
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-11-27 13:26:35 +0100

    add methods to add and remove contact person from a supplier

--- app/controllers/supplier.py
+++ app/controllers/supplier.py
@@ -1,4 +1,5 @@
 from app.controllers import BaseController
+from app.data.models.contact_person import ContactPerson
 from app.data.models.supplier import Supplier
 from app.data.repositories.supplier import SupplierRepository
 
@@ -9,3 +10,12 @@ class SupplierController(BaseController):
     @staticmethod
     def find_by_id(_id: int) -> Supplier:
         return SupplierRepository.find_by_id(_id)
+
+    @staticmethod
+    def add_contact_person(supplier: Supplier, contact_person: ContactPerson,
+                           remove_existing: bool = False) -> None:
+        SupplierRepository.add_contact_person(supplier, contact_person, remove_existing)
+
+    @staticmethod
+    def remove_contact_person(supplier: Supplier) -> None:
+        SupplierRepository.remove_contact_person(supplier)
--- app/data/_mysql/repositories/supplier.py
+++ app/data/_mysql/repositories/supplier.py
@@ -1,9 +1,31 @@
 from app.data._mysql.db import session
-from app.data._mysql.repositories import BaseRepository
+from app.data._mysql.models import ContactPerson
 from app.data._mysql.models.supplier import Supplier
+from app.data._mysql.repositories import BaseRepository
+from app.data._mysql.repositories.contact_person import ContactPersonRepository
 
 
 class SupplierRepository(BaseRepository):
     @staticmethod
     def find_by_id(_id: int) -> Supplier:
         return session.query(Supplier).filter_by(supplier_id=_id).first()
+
+    @staticmethod
+    def add_contact_person(supplier: Supplier, contact_person: ContactPerson,
+                           remove_existing: bool = False) -> None:
+        if remove_existing:
+            SupplierRepository.remove_contact_person(supplier)
+
+        supplier.contact_person_id = contact_person.contact_person_id
+        contact_person.supplier = supplier
+        session.commit()
+
+    @staticmethod
+    def remove_contact_person(supplier: Supplier) -> None:
+        if not supplier.contact_person_id:
+            raise ValueError("Supplier has no contact person.")
+
+        contact_person = ContactPersonRepository.find_by_id(supplier.contact_person_id)
+        contact_person.supplier = None
+        supplier.contact_person_id = None
+        session.commit()
