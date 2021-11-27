d4e589bc00e744715ea2f00824b7cbe60dc63e8e
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-11-27 15:03:02 +0100

    add methods for adding and removing supplier to/from store

--- app/controllers/store.py
+++ app/controllers/store.py
@@ -1,5 +1,6 @@
 from app.controllers import BaseController
 from app.data.models.store import Store
+from app.data.models.supplier import Supplier
 from app.data.repositories.store import StoreRepository
 
 
@@ -9,3 +10,11 @@ class StoreController(BaseController):
     @staticmethod
     def find_by_id(_id: int) -> Store:
         return StoreRepository.find_by_id(_id)
+
+    @staticmethod
+    def add_supplier_to_store(store: Store, supplier: Supplier) -> None:
+        StoreRepository.add_supplier_to_store(store, supplier)
+
+    @staticmethod
+    def remove_supplier_from_store(store: Store, supplier: Supplier) -> None:
+        StoreRepository.remove_supplier_from_store(store, supplier)
--- app/data/_mysql/repositories/store.py
+++ app/data/_mysql/repositories/store.py
@@ -1,4 +1,5 @@
 from app.data._mysql.db import session
+from app.data._mysql.models import Supplier
 from app.data._mysql.repositories import BaseRepository
 from app.data._mysql.models.store import Store
 
@@ -7,3 +8,24 @@ class StoreRepository(BaseRepository):
     @staticmethod
     def find_by_id(_id: int) -> Store:
         return session.query(Store).filter_by(store_id=_id).first()
+
+    @staticmethod
+    def add_supplier_to_store(store: Store, supplier: Supplier) -> None:
+        if StoreRepository.has_supplier(store, supplier):
+            return
+        store.suppliers.append(supplier)
+        session.commit()
+
+    @staticmethod
+    def remove_supplier_from_store(store: Store, supplier: Supplier) -> None:
+        if not StoreRepository.has_supplier(store, supplier):
+            return
+        store.suppliers.remove(supplier)
+        session.commit()
+
+    @staticmethod
+    def has_supplier(store: Store, supplier: Supplier) -> bool:
+        for supp in store.suppliers:
+            if supp.supplier_id == supplier.supplier_id:
+                return True
+        return False
