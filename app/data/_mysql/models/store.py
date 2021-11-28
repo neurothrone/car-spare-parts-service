b6ba265c83b06d4d4635117f30513cb29cefa188
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-11-28 07:25:34 +0100

    remove redundant code and add new columns

--- app/data/_mysql/models/store.py
+++ app/data/_mysql/models/store.py
@@ -18,44 +18,20 @@ class Store(BaseModel):
     phone = Column(String(length=25), nullable=False)
     email = Column(String(length=100), nullable=False)
 
+    address = Column(String(length=100), nullable=True)
+    zip_code = Column(String(length=7), nullable=True)
+    city = Column(String(length=45), nullable=True)
+
     employees = relationship("Employee", back_populates="store")
-
     products = relationship("StoreHasProduct", back_populates="store")
-
     suppliers = relationship(
         "Supplier",
         secondary=stores_has_suppliers,
         back_populates="stores"
     )
 
-    # UNIQUE (store_id, store_type)
-
-
-class OnlineStore:
-    pass
-
-
-class PhysicalStore:
-    pass
-
-# class OnlineStore(BaseModel):
-#     __tablename__ = "online_stores"
-#
-#     store_id = Column(Integer, autoincrement=True, primary_key=True)
-#     store_type = Column(CHAR(length=1), nullable=False)
-#
-#     # FOREIGN KEY (store_id, store_type)
-#     #    REFERENCES stores (store_id, store_type) ON DELETE CASCADE
-#
-#
-# class PhysicalStore(BaseModel):
-#     __tablename__ = "physical_stores"
-#
-#     store_id = Column(Integer, autoincrement=True, primary_key=True)
-#     store_type = Column(CHAR(length=1), nullable=False)
-#     address = Column(String(length=125), nullable=False)
-#     zip_code = Column(String(length=7), nullable=False)
-#     city = Column(String(length=50), nullable=False)
-#
-#     # FOREIGN KEY (store_id, store_type)
-#     #    REFERENCES stores (store_id, store_type) ON DELETE CASCADE
+# TODO:
+#       - remove Not Null constraint from address, zip_code, city
+#       - when creating a store, if store_type is online, only then can address, zip and city be null
+#       - update repository, controller, generator and add safeguard/differentiation for online, physical type
+#       - verify new changes work on generators and so on
