86e276a08713807e72ca9386ac07876e8192dab4
Mahboubeh Azizzadeh <m.a.82@outlook.com>
2021-11-30 12:45:49 +0100

    Create classes and relationships

--- app/data/_mysql/models/manufacturer.py
+++ app/data/_mysql/models/manufacturer.py
@@ -1,12 +1,16 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
+from app.data._mysql.db import Base
+from sqlalchemy.orm import relationship
 
-from app.data._mysql.models import BaseModel
 
-
-class Manufacturer(BaseModel):
+class ManuFacturer(Base):
     __tablename__ = "manufacturers"
 
     manufacturer_id = Column(Integer, autoincrement=True, primary_key=True)
     company_name = Column(String(45), nullable=False)
     head_office_phone = Column(String(25), nullable=False)
     head_office_address = Column(String(100), nullable=False)
+    contact_person_id = Column(Integer, ForeignKey('contact_persons.contact_person_id'))
+
+    contact_person = relationship('ContactPerson', back_populates="manufacturers")
+    products = relationship('ProductHasManufacturer', back_populates='manufacturer')
