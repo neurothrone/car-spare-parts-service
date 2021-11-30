79db9f1df675fe333afb7c4087fe5070f060a069
Mahboubeh Azizzadeh <m.a.82@outlook.com>
2021-11-30 12:45:49 +0100

    Create classes and relationships

--- app/data/_mysql/db/__init__.py
+++ app/data/_mysql/db/__init__.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base, DeclarativeMeta, sessionmaker, Session
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from app.config import Config
 
 engine = create_engine(url=Config.get_base_config())
 
-Base: DeclarativeMeta = declarative_base()
-session_maker: sessionmaker = sessionmaker()
-session_maker.configure(bind=engine)
-session: Session = session_maker()
+
+Base = declarative_base()
+Session = sessionmaker()
+Session.configure(bind=engine)
+session = Session()
