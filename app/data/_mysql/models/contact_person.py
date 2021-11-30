b53eb86112d236965aba4656a763701a38105bfd
Mahboubeh Azizzadeh <m.a.82@outlook.com>
2021-11-30 12:45:49 +0100

    update relationships and add more code

--- app/data/_mysql/models/contact_person.py
+++ app/data/_mysql/models/contact_person.py
@@ -1,10 +1,9 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
+from app.data._mysql.models import Base
 
-from app.data._mysql.models import BaseModel
 
-
-class ContactPerson(BaseModel):
+class ContactPerson(Base):
     __tablename__ = "contact_persons"
 
     contact_person_id = Column(Integer, autoincrement=True, primary_key=True)
@@ -13,4 +12,8 @@ class ContactPerson(BaseModel):
     phone = Column(String(25), nullable=False)
     email = Column(String(100), nullable=False)
 
-    supplier = relationship("Supplier", back_populates="contact_person", uselist=False)
+    # supplier = relationship("Supplier", backref="contact_person", uselist=False)
+    # manufacturer = relationship("ManuFacturer", backref="contact_person", uselist=False)
+
+    # supplier = relationship("Supplier", back_populates="contact_persons")
+    # manufacturer = relationship("ManuFacturer", back_populates="contact_persons")
