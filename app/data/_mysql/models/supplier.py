b9ea5a02e7718a6a2067bd0e6f4601184f37d751
Mahboubeh Azizzadeh <m.a.82@outlook.com>
2021-12-01 16:45:43 +0100

    ...

--- app/data/_mysql/models/supplier.py
+++ app/data/_mysql/models/supplier.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from app.data._mysql.models import Base
+from app.data._mysql.models import BaseModel
 
 
-class Supplier(Base):
+class Supplier(BaseModel):
     __tablename__ = "suppliers"
 
     supplier_id = Column(Integer, autoincrement=True, primary_key=True)
@@ -12,6 +12,6 @@ class Supplier(Base):
     head_office_address = Column(String(100), nullable=False)
     contact_person_id = Column(Integer, ForeignKey("contact_persons.contact_person_id"), nullable=True)
 
-    contact_person = relationship("ContactPerson", back_populates="supplier")
+    contact_person = relationship("ContactPerson", back_populates="supplier", uselist=False)
     products = relationship("ProductHasSupplier", back_populates="supplier")
     stores = relationship("StoreHasSupplier", back_populates="supplier")
