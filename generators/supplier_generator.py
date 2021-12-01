811af44eece7da7b6a6d3a786860f709beb2bd51
Mahboubeh Azizzadeh <m.a.82@outlook.com>
2021-12-01 16:45:43 +0100

    ...

--- generators/supplier_generator.py
+++ generators/supplier_generator.py
@@ -1,7 +1,5 @@
-from random import randint
-
 from app.controllers.supplier_controller import SupplierController
-from generators.fake_data import FakeData
+from app.data.models.supplier import Supplier
 from shared.validators import validate_length
 
 
@@ -11,44 +9,25 @@ class SupplierGenerator:
     HEAD_OFFICE_ADDRESS_LEN = 100
 
     @staticmethod
-    def generate(company_name: str,
-                 head_office_phone: str,
-                 head_office_address: str) -> None:
-        validate_length(provided=company_name,
-                        limit=SupplierGenerator.COMPANY_NAME_MAX_LEN)
-        validate_length(provided=head_office_phone,
-                        limit=SupplierGenerator.HEAD_OFFICE_PHONE_LEN)
-        validate_length(provided=head_office_address,
-                        limit=SupplierGenerator.HEAD_OFFICE_ADDRESS_LEN)
+    def generate(company_name: str, head_office_phone: str, head_office_address: str) -> Supplier:
+        validate_length(provided=company_name, limit=SupplierGenerator.COMPANY_NAME_MAX_LEN)
+        validate_length(provided=head_office_phone, limit=SupplierGenerator.HEAD_OFFICE_PHONE_LEN)
+        validate_length(provided=head_office_address, limit=SupplierGenerator.HEAD_OFFICE_ADDRESS_LEN)
 
-        cpi = randint(1, 101)
-        if cpi >= 80:
-            contact_person_id = None
-        else:
-            contact_person_id = cpi
-
-        SupplierController.create(
-            company_name=company_name,
-            head_office_phone=head_office_phone,
-            head_office_address=head_office_address,
-            contact_person_id=contact_person_id)
-
-    @classmethod
-    def populate_database(cls, amount: int) -> None:
-        phone_numbers = FakeData.generate_phone_numbers(amount)
-        locations = FakeData.generate_locations(amount)
-        company_names = FakeData.generate_companies(amount)
-
-        for i in range(amount):
-            cls.generate(company_name=company_names[i],
-                         head_office_phone=phone_numbers[i],
-                         head_office_address=locations[i].__str__())
-
-        print("----- Suppliers generated -----")
+        return SupplierController.create(
+            company_name=company_name, head_office_phone=head_office_phone,
+            head_office_address=head_office_address)
 
 
 def main():
-    SupplierGenerator.populate_database(amount=100)
+    supplier = SupplierGenerator.generate(
+        company_name="Supply Parts AB",
+        head_office_phone="+64 73 944 71 23",
+        head_office_address="Karbegsgatan 23, 173 43 Nattberg"
+    )
+
+    SupplierController.pprint(supplier)
+    SupplierController.pprint_all()
 
 
 if __name__ == "__main__":
