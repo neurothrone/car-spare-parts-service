465ede2698ea5e9a49409010f5839018102ae3da
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-01 06:55:51 +0100

    complete and test fake data generation for date, color and license plate

--- generators/fake_data.py
+++ generators/fake_data.py
@@ -1,9 +1,12 @@
+import datetime
 from random import randint
 
 from faker import Faker
+
+
 # from faker.providers import address
-from faker.providers import company
-from faker.exceptions import UniquenessException
+# from faker.providers import company
+# from faker.exceptions import UniquenessException
 
 
 class Location:
@@ -13,61 +16,41 @@ class Location:
         self.zip_code = zip_code
         self.city = city
 
-    def __str__(self) -> str:
-        pass
-
     def __repr__(self) -> str:
-        pass
+        return f"Location(address={self.address}, street_no={self.street_no}," \
+               f" zip_code={self.zip_code}, city={self.city})"
 
-
-class FakeStoreGenerator:
-
-    @staticmethod
-    def generate_store():
-        # store_type, o, p
-        # phone
-        # email
-        # address
-        # zip code
-        # city
-
-        pass
+    def __str__(self) -> str:
+        return f"{self.address} {self.street_no}\n{self.zip_code} {self.city}"
 
 
 class FakeData:
-    faker: Faker = Faker("sv_SE")
+    _faker: Faker = Faker("sv_SE")
+
+    MIN_AGE = 18
+    MAX_AGE = 72
 
     @classmethod
     def configure_locale(cls, locale: str) -> None:
-        cls.faker = Faker(locale=locale)
+        cls._faker = Faker(locale=locale)
 
     @staticmethod
     def generate_random_number(min_: int, max_: int) -> int:
         return randint(min_, max_)
 
-    @staticmethod
-    def generate_full_names(amount: int, unique: bool = True) -> list[str]:
-        faker = Faker("sv_SE")
-
+    @classmethod
+    def generate_full_names(cls, amount: int, unique: bool = True) -> list[str]:
         if unique:
             full_names_set = set()
             while len(full_names_set) < amount:
-                full_names_set.add(faker.name())
+                full_names_set.add(cls._faker.name())
             return list(full_names_set)
 
-        return [faker.name() for _ in range(amount)]
+        return [cls._faker.name() for _ in range(amount)]
 
-    @staticmethod
-    def generate_phone_number() -> str:
-        # +64 7X XXX XX XX
-        #      012345678
-        phone = "+64 7"
-        for i in range(11):
-            if i in [1, 5, 8]:
-                phone += " "
-            else:
-                phone += str(FakeData.generate_random_number(min_=0, max_=9))
-        return phone
+    @classmethod
+    def generate_phone_number(cls) -> str:
+        return cls._faker.phone_number()
 
     @staticmethod
     def generate_phone_numbers(amount: int) -> list[str]:
@@ -80,15 +63,15 @@ class FakeData:
     def generate_email(username: str, domain_name: str = "example", domain: str = "se") -> str:
         return f"{username}@{domain_name}.{domain}"
 
-    @staticmethod
-    def generate_emails(full_names: list[str], amount: int,
+    @classmethod
+    def generate_emails(cls, full_names: list[str], amount: int,
                         domain_name: str = "example", domain: str = "se") -> list[str]:
         emails = list()
         if amount > len(full_names):
             raise ValueError("Amount needs to be equal to or less than the length of full_names.")
         for i in range(amount):
             first_name, last_name = full_names[i].split(" ")
-            email = FakeData.generate_email(
+            email = cls.generate_email(
                 username=f"{first_name.lower()}.{last_name.lower()}",
                 domain_name=domain_name,
                 domain=domain
@@ -98,14 +81,53 @@ class FakeData:
 
     @classmethod
     def generate_location(cls) -> Location:
-        lines = cls.faker.address().split("\n")
+        lines = cls._faker.address().split("\n")
         address, street_no = lines[0].split(" ")
         zip_code, city = lines[1].split(" ")
         return Location(address=address, street_no=int(street_no), zip_code=int(zip_code), city=city)
 
     @classmethod
     def generate_locations(cls, amount: int) -> list[Location]:
-        return [FakeData.generate_location() for _ in range(amount)]
+        return [cls.generate_location() for _ in range(amount)]
+
+    @classmethod
+    def generate_license_plate(cls) -> str:
+        return cls._faker.license_plate()
+
+    @classmethod
+    def generate_license_plates(cls, amount: int) -> list[str]:
+        license_plates = set()
+        while len(license_plates) < amount:
+            license_plates.add(FakeData.generate_license_plate())
+        return list(license_plates)
+
+    @classmethod
+    def generate_color_name(cls) -> str:
+        return cls._faker.safe_color_name()
+
+    @classmethod
+    def generate_15_color_names(cls) -> list[str]:
+        max_colors = 15
+        colors = set()
+        while len(colors) < max_colors:
+            colors.add(FakeData.generate_color_name())
+        return list(colors)
+
+    @classmethod
+    def generate_date(cls) -> datetime.date:
+        return cls._faker.date_of_birth(minimum_age=FakeData.MIN_AGE, maximum_age=FakeData.MAX_AGE)
+
+    @classmethod
+    def generates_dates(cls, amount: int) -> list[datetime.date]:
+        return [cls.generate_date() for _ in range(amount)]
+
+    @classmethod
+    def generate_car(cls):
+        pass
+
+    @classmethod
+    def generate_cars(cls):
+        pass
 
 
 def print_sequence(sequence: list) -> None:
@@ -113,19 +135,51 @@ def print_sequence(sequence: list) -> None:
         print(item)
 
 
-def main():
-    FakeData.generate_address()
-
+def test_name_generation():
     full_names = FakeData.generate_full_names(500)
     print_sequence(full_names)
 
-    # emails = FakePersonGenerator.generate_emails(full_names, 500)
-    # print_sequence(emails)
 
-    # phone_numbers = FakePersonGenerator.generate_phone_numbers(500)
-    # print_sequence(phone_numbers)
+def test_email_generation():
+    full_names = FakeData.generate_full_names(500)
+    emails = FakeData.generate_emails(full_names, 500)
+    print_sequence(emails)
 
-    # print(FakePersonGenerator.generate_phone_number())
+
+def test_phone_generation():
+    phone_numbers = FakeData.generate_phone_numbers(500)
+    print_sequence(phone_numbers)
+
+
+def test_address_generation():
+    locations = FakeData.generate_locations(500)
+    print_sequence(locations)
+
+
+def test_reg_no_generation():
+    # print(FakeData.generate_license_plate())
+    license_plates = FakeData.generate_license_plates(500)
+    print_sequence(license_plates)
+
+
+def test_color_generation():
+    # print(FakeData.generate_color_name())
+    print_sequence(FakeData.generate_15_color_names())
+
+
+def test_date_generation():
+    # print(FakeData.generate_date())
+    print_sequence(FakeData.generates_dates(500))
+
+
+def main():
+    # test_name_generation()
+    # test_email_generation()
+    # test_phone_generation()
+    # test_address_generation()
+    # test_reg_no_generation()
+    # test_color_generation()
+    test_date_generation()
 
 
 if __name__ == "__main__":
