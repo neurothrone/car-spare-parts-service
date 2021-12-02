e09a54fc15b92022a88de5649b930c06f66ef6a2
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-02 07:47:34 +0100

    add logic to change store employees work at and to reset all

--- app/controllers/employee_controller.py
+++ app/controllers/employee_controller.py
@@ -1,4 +1,9 @@
+from random import randint
+from typing import Optional
+
 from app.controllers import BaseController
+from app.controllers.store_controller import StoreController
+from app.data.models.store import Store, StoreType
 from app.data.models.employee import Employee
 from app.data.repositories.employee_repository import EmployeeRepository
 
@@ -8,7 +13,7 @@ class EmployeeController(BaseController):
 
     @classmethod
     def create(cls, first_name: str, last_name: str, phone: str, email: str,
-               store_id: int = None) -> None:
+               store_id: Optional[int] = None) -> None:
         EmployeeRepository.create(cls.model,
                                   first_name=first_name,
                                   last_name=last_name,
@@ -19,3 +24,50 @@ class EmployeeController(BaseController):
     @staticmethod
     def find_by_id(_id: int) -> Employee:
         return EmployeeRepository.find_by_id(_id)
+
+    @staticmethod
+    def change_store(employee: Employee, store_id: Optional[int]) -> None:
+        if store_id is not None:
+            if store_id < 1:
+                raise ValueError("Invalid value for store_id. Can not be zero or negative.")
+
+            store = StoreController.find_by_id(store_id)
+
+            if store.store_type == StoreType.ONLINE:
+                raise ValueError("Employee can't work at online store.")
+
+            if employee.store_id == store_id:
+                raise ValueError("Employee is already working there.")
+
+        EmployeeRepository.change_store(employee, store_id)
+
+    @classmethod
+    def connect_employees_to_stores(cls, min_: int, max_: int) -> None:
+        employees: list[Employee] = cls.find_all()
+        stores: list[Store] = StoreController.find_all()
+        total_employees = len(employees)
+        total_stores = len(stores)
+
+        employee_index = 0
+        store_index = 0
+
+        while employee_index < total_employees and store_index < total_stores:
+            employees_in_store = randint(min_, max_)
+
+            try:
+                for _ in range(employees_in_store):
+                    cls.change_store(
+                        employee=employees[employee_index],
+                        store_id=stores[store_index].store_id)
+                    employee_index += 1
+            except IndexError:
+                break
+
+            store_index += 1
+
+    @classmethod
+    def reset_all_employees_store(cls):
+        employees: list[Employee] = cls.find_all()
+
+        for employee in employees:
+            cls.change_store(employee, None)
