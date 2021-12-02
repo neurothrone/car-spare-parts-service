9b83aeec271d452b6411a4580077baec1d0ab61d
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-02 11:25:21 +0100

    simplify store database population

--- generators/_populate_db.py
+++ generators/_populate_db.py
@@ -5,13 +5,13 @@ from generators.store_generator import StoreGenerator
 
 def populate_db():
     StoreGenerator.populate_database(amount=100)
-    StoreGenerator.generate_online_store()
     EmployeeGenerator.populate_database(amount=100)
 
 
 def main():
-    # EmployeeController.connect_employees_to_stores(min_=1, max_=3)
-    EmployeeController.reset_all_employees_store()
+    # populate_db()
+    EmployeeController.connect_employees_to_stores(min_=1, max_=3)
+    # EmployeeController.reset_all_employees_store()
 
 
 if __name__ == "__main__":
