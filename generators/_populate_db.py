df339b98d7c9cd6c21be25a9d70f0f4d6880a695
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-02 19:26:21 +0100

    add add_products_to_stores() method call to populate_db() method

--- generators/_populate_db.py
+++ generators/_populate_db.py
@@ -1,4 +1,3 @@
-from app.controllers.employee_controller import EmployeeController
 from generators.customer_generator import CustomerGenerator
 from generators.employee_generator import EmployeeGenerator
 from generators.product_generator import ProductGenerator
@@ -10,12 +9,18 @@ def populate_db():
     EmployeeGenerator.populate_database(amount=100)
     CustomerGenerator.populate_database(amount=100)
     ProductGenerator.populate_database(amount=100)
+    StoreGenerator.add_products_to_stores(min_per_store=1, max_per_store=5)
 
 
 def main():
     # populate_db()
-    EmployeeController.connect_employees_to_stores(min_=1, max_=3)
+    # EmployeeController.connect_employees_to_stores(min_=1, max_=3)
     # EmployeeController.reset_all_employees_store()
+
+    # StoreGenerator.populate_database(amount=100)
+    # StoreGenerator.add_products_to_stores(min_per_store=1, max_per_store=5)
+    StoreGenerator.print_products_in_stores()
+    # StoreGenerator.remove_all_products_in_stores()
 
 
 if __name__ == "__main__":
