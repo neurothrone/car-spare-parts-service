3dd823fae7f325d88c18a3c6d36a4c789b81cf06
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-02 12:46:59 +0100

    add method to populate products table in database

--- generators/product_generator.py
+++ generators/product_generator.py
@@ -1,17 +1,18 @@
 from app.controllers.product_controller import ProductController
+from generators.fake_data import FakeData
 
 
 class ProductGenerator:
     NAME_MAX_LEN = 45
     DESCRIPTION_MAX_LEN = 255
 
-    @staticmethod
-    def generate(name: str, description: str, cost: float, price: float) -> None:
+    @classmethod
+    def generate(cls, name: str, description: str, cost: float, price: float) -> None:
         if len(name) > ProductGenerator.NAME_MAX_LEN:
-            raise ValueError(f"Max length of name is {ProductGenerator.NAME_MAX_LEN}.")
+            raise ValueError(f"Max length of name is {cls.NAME_MAX_LEN}.")
 
         if len(description) > ProductGenerator.DESCRIPTION_MAX_LEN:
-            raise ValueError(f"Max length of name is {ProductGenerator.DESCRIPTION_MAX_LEN}.")
+            raise ValueError(f"Max length of name is {cls.DESCRIPTION_MAX_LEN}.")
 
         if cost > price:
             raise ValueError(f"Cost is greater than price.")
@@ -19,17 +20,22 @@ class ProductGenerator:
         ProductController.create(
             name=name, description=description, cost=cost, price=price)
 
+    @classmethod
+    def populate_database(cls, amount: int) -> None:
+        for _ in range(amount):
+            product = FakeData.generate_product(cost_scale=1)
+
+            cls.generate(
+                name=product.name,
+                description=product.description,
+                cost=product.cost,
+                price=product.price)
+
+        print(f"----- {amount} Products generated -----")
+
 
 def main():
-    ProductGenerator.generate(
-        name="Part D",
-        description="Description about product",
-        cost=25,
-        price=30
-    )
-
-    product = ProductController.find_by_id(2)
-    print(product)
+    ProductGenerator.populate_database(amount=500)
 
 
 if __name__ == "__main__":
