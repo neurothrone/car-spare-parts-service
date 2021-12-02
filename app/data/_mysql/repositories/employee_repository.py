8f4ac04c32a498a365643dd3c8b415adb88999da
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-02 07:45:31 +0100

    add change_store() method

--- app/data/_mysql/repositories/employee_repository.py
+++ app/data/_mysql/repositories/employee_repository.py
@@ -7,3 +7,8 @@ class EmployeeRepository(BaseRepository):
     @staticmethod
     def find_by_id(_id: int) -> Employee:
         return session.query(Employee).filter_by(employee_id=_id).first()
+
+    @classmethod
+    def change_store(cls, employee: Employee, store_id: int) -> None:
+        employee.store_id = store_id
+        cls.save_to_db(employee)
