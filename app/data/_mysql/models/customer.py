4f82f3d2292aea4039fba3e336f8e476df2a88c6
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-03 07:29:19 +0100

    update name columns to be nullable

--- app/data/_mysql/models/customer.py
+++ app/data/_mysql/models/customer.py
@@ -14,17 +14,15 @@ class Customer(BaseModel):
 
     customer_id = Column(Integer, autoincrement=True, primary_key=True)
     customer_type = Column(CHAR(length=1), nullable=False)  # CHECK (customer_type IN ('c', 'p'))
-    customer_name = Column(String(length=100), nullable=False)
-    contact_first_name = Column(String(length=45), nullable=False)
-    contact_last_name = Column(String(length=45), nullable=False)
+    customer_name = Column(String(length=100), nullable=True)
+    contact_first_name = Column(String(length=45), nullable=True)
+    contact_last_name = Column(String(length=45), nullable=True)
     phone = Column(String(length=25), nullable=False)
     email = Column(String(length=100), nullable=False)
     address = Column(String(length=100), nullable=False)
     zip_code = Column(String(length=7), nullable=False)
     city = Column(String(length=50), nullable=False)
 
-    # employee (optional) One-to-Many (optional) customer
-    # reference to seller
     employee_id = Column(Integer, ForeignKey("employees.employee_id"))
     employee = relationship("Employee", back_populates="customers")
 
