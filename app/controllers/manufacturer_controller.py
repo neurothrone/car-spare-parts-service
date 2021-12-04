5f800a4c270953f9bbf6967de0e2bb8c95248f13
Mahboubeh Azizzadeh <m.a.82@outlook.com>
2021-12-04 07:50:17 +0100

    add some tests

--- app/controllers/manufacturer_controller.py
+++ app/controllers/manufacturer_controller.py
@@ -1,6 +1,7 @@
 from app.controllers import BaseController
 from app.data.models.manufacturer import Manufacturer
 from app.data.repositories.manufacturer_repository import ManufacturerRepository
+from app.data.models.contact_person import ContactPerson
 
 
 class ManufacturerController(BaseController):
@@ -9,3 +10,11 @@ class ManufacturerController(BaseController):
     @staticmethod
     def find_by_id(_id: int) -> Manufacturer:
         return ManufacturerRepository.find_by_id(_id)
+
+    @staticmethod
+    def add_contact_person(manufacturer: Manufacturer, contact_person: ContactPerson) -> None:
+        ManufacturerRepository.add_contact_person(manufacturer, contact_person)
+
+    @staticmethod
+    def remove_contact_person(manufacturer: Manufacturer) -> None:
+        ManufacturerRepository.remove_contact_person(manufacturer)
