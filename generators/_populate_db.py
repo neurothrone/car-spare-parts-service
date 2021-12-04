673edcb6173c51a07d14d3887319116c9c60a661
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-04 14:09:15 +0100

    add new changes to test if new mysql db name and config class work

--- generators/_populate_db.py
+++ generators/_populate_db.py
@@ -1,3 +1,4 @@
+from app.controllers.employee_controller import EmployeeController
 from generators.customer_generator import CustomerGenerator
 from generators.employee_generator import EmployeeGenerator
 from generators.product_generator import ProductGenerator
@@ -7,19 +8,23 @@ from generators.store_generator import StoreGenerator
 def populate_db():
     StoreGenerator.populate_database(amount=100)
     EmployeeGenerator.populate_database(amount=100)
-    CustomerGenerator.populate_database(amount=100)
-    ProductGenerator.populate_database(amount=100)
-    StoreGenerator.add_products_to_stores(min_per_store=1, max_per_store=5)
+    # dependent on above
+    EmployeeController.connect_employees_to_stores(min_=1, max_=3)
+
+    # CustomerGenerator.populate_database(amount=100)
+
+    # ProductGenerator.populate_database(amount=100)
+    # StoreGenerator.add_products_to_stores(min_per_store=1, max_per_store=5)
 
 
 def main():
-    # populate_db()
+    populate_db()
     # EmployeeController.connect_employees_to_stores(min_=1, max_=3)
     # EmployeeController.reset_all_employees_store()
 
     # StoreGenerator.populate_database(amount=100)
     # StoreGenerator.add_products_to_stores(min_per_store=1, max_per_store=5)
-    StoreGenerator.print_products_in_stores()
+    # StoreGenerator.print_products_in_stores()
     # StoreGenerator.remove_all_products_in_stores()
 
 
