9ca15cdbe19410580c68a1f606f600077c85ddc1
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-04 13:32:52 +0100

    create config base class and subclass for mongo and mysql databases

--- app/config.py
+++ app/config.py
@@ -1,18 +1,84 @@
-import os
+"""
+Config classes and logic for reading environment variables
+for mongo and mysql databases.
 
-from dotenv import load_dotenv
+Create two files in the same directory as this file 'config.py' and name them:
+.env.mongo
+.env.mysql
 
-load_dotenv()
+Write down these variables in the files and replace ... with the corresponding values:
+DB_NAME=...
+DB_PROTOCOL=...
+DB_USER=...
+DB_PASS=...
+HOST=...
+PORT=...
+"""
+
+from abc import ABC, abstractmethod
+
+from dotenv import dotenv_values
 
 
-class Config:
-    DB_NAME = os.getenv("DB_NAME")
-    DB_PROTOCOL = os.getenv("DB_PROTOCOL")
-    DB_USER = os.getenv("DB_USER")
-    DB_PASS = os.getenv("DB_PASS")
-    HOST = os.getenv("HOST")
-    PORT = os.getenv("PORT")
+class Config(ABC):
+    _data: dict
+
+    DB_NAME: str
+    DB_PROTOCOL: str
+    DB_USER: str
+    DB_PASS: str
+    HOST: str
+    PORT: str
 
     @classmethod
-    def get_base_config(cls) -> str:
+    def read_env_values(cls) -> None:
+        cls.DB_NAME: str = cls._data.get("DB_NAME", None)
+        cls.DB_PROTOCOL: str = cls._data.get("DB_PROTOCOL", None)
+        cls.DB_USER: str = cls._data.get("DB_USER", None)
+        cls.DB_PASS: str = cls._data.get("DB_PASS", None)
+        cls.HOST: str = cls._data.get("HOST", None)
+        cls.PORT: str = cls._data.get("PORT", None)
+
+    @abstractmethod
+    def base_config(self) -> str:
+        pass
+
+    @abstractmethod
+    def test_config(self) -> str:
+        pass
+
+
+class MongoConfig(Config):
+    _data = {**dotenv_values(".env.mongo")}
+
+    @classmethod
+    def base_config(cls) -> str:
+        cls.read_env_values()
+        return f"{cls.DB_PROTOCOL}://{cls.DB_USER}:{cls.DB_PASS}@{cls.HOST}:{cls.PORT}"
+
+    @classmethod
+    def test_config(cls) -> str:
+        raise NotImplementedError("test_config() is not implemented.")
+
+
+class MysqlConfig(Config):
+    _data = {**dotenv_values(".env.mysql")}
+
+    @classmethod
+    def base_config(cls) -> str:
+        cls.read_env_values()
         return f"{cls.DB_PROTOCOL}://{cls.DB_USER}:{cls.DB_PASS}@{cls.HOST}:{cls.PORT}/{cls.DB_NAME}"
+
+    @classmethod
+    def test_config(cls) -> str:
+        raise NotImplementedError("test_config() is not implemented.")
+
+
+def main():
+    print(MongoConfig.base_config())
+    print(MongoConfig.DB_NAME)
+    print(MysqlConfig.base_config())
+
+
+if __name__ == "__main__":
+    main()
