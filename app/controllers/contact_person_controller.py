d2b750d8dc188d8901db5ca6aae514573a69891f
Mahboubeh Azizzadeh <m.a.82@outlook.com>
2021-12-04 12:00:27 +0100

    add randint for contact_person_id

--- app/controllers/contact_person_controller.py
+++ app/controllers/contact_person_controller.py
@@ -9,3 +9,11 @@ class ContactPersonController(BaseController):
     @staticmethod
     def find_by_id(_id: int) -> ContactPerson:
         return ContactPersonRepository.find_by_id(_id)
+
+    @classmethod
+    def create(cls, first_name: str, last_name: str, phone: str, email: str = None) -> None:
+        ContactPersonRepository.create(cls.model,
+                                       first_name=first_name,
+                                       last_name=last_name,
+                                       phone=phone,
+                                       email=email)
--- app/data/_mysql/repositories/contact_person_repository.py
+++ app/data/_mysql/repositories/contact_person_repository.py
@@ -7,3 +7,11 @@ class ContactPersonRepository(BaseRepository):
     @staticmethod
     def find_by_id(_id: int) -> ContactPerson:
         return session.query(ContactPerson).filter_by(contact_person_id=_id).first()
+
+    @staticmethod
+    def find_by_first_name(first_name: str) -> ContactPerson:
+        return session.query(ContactPerson).filter_by(first_name=first_name).first()
+
+    @staticmethod
+    def find_by_last_name(last_name: str) -> ContactPerson:
+        return session.query(ContactPerson).filter_by(last_name=last_name).first()
