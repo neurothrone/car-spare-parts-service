00e7fce051550d38becdaf64c3b6f96560b02bfd
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-04 14:07:39 +0100

    modify mysql url to new config class method

--- app/data/_mysql/db/__init__.py
+++ app/data/_mysql/db/__init__.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker, Session
 
-from app.config import Config
+from app.config import MysqlConfig
 
-engine = create_engine(url=Config.get_base_config())
+engine = create_engine(url=MysqlConfig.base_config())
 
 Base = declarative_base()
 session_maker: sessionmaker = sessionmaker()
