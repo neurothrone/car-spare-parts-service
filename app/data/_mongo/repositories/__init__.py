dca7d5ff245aba78122253216598d2d3ab5e9a90
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-05 14:08:39 +0100

    create BaseRepository class for mongo db models

---
+++ app/data/_mongo/repositories/__init__.py
@@ -0,0 +1,69 @@
+from __future__ import annotations
+from abc import ABC
+from typing import Optional, Type
+
+import bson.errors
+from bson import ObjectId
+
+from app.data._mongo.models import ResultList, T
+
+
+class BaseRepository(ABC):
+    model: Type[T]
+
+    @classmethod
+    def create(cls, **kwargs) -> None:
+        obj = cls.model(**kwargs)
+        obj.save()
+
+    @classmethod
+    def create_many(cls, data: list[dict]) -> int:
+        insertions = cls.model.collection.insert_many(data)
+        return len(insertions.inserted_ids)
+
+    @classmethod
+    def delete_by(cls, query: Optional[dict] = None, many: bool = True) -> int:
+        if many:
+            result = cls.model.collection.delete_many(query if query else {})
+        else:
+            result = cls.model.collection.delete_one(query if query else {})
+        return result.deleted_count
+
+    @classmethod
+    def delete_by_regex(cls, attribute: str, regex: str, many: bool = True) -> int:
+        query = {attribute: {"$regex": regex}}
+        return cls.delete_by(query, many)
+
+    @classmethod
+    def delete_all(cls) -> int:
+        return cls.delete_by()
+
+    @classmethod
+    def find(cls, many: bool = False, **kwargs) -> Optional[T | list[T]]:
+        result = ResultList(cls.model(**item) for item in cls.model.collection.find(kwargs))
+        if many:
+            return result
+        return result.first_or_none()
+
+    @classmethod
+    def find_by_id(cls, _id: str) -> Optional[T]:
+        try:
+            return cls.find(_id=ObjectId(_id))
+        except bson.errors.InvalidId:
+            print(f"Error (bson.errors.InvalidId): That is not a valid "
+                  f"ObjectId in function -> {cls.find_by_id.__name__}() "
+                  f"on class -> {cls.__name__}.")
+
+    @classmethod
+    def find_all(cls, **kwargs) -> Optional[list[T]]:
+        return cls.find(many=True, **kwargs)
+
+    @classmethod
+    def find_all_sort_by(cls, attribute: str, ascending: bool = True,
+                         limit: Optional[int] = None) -> Optional[T | list[T]]:
+        sort_in = 1 if ascending else -1
+        if limit:
+            return ResultList(cls.model(**item) for item in
+                              cls.model.collection.find().sort(attribute, sort_in).limit())
+        return ResultList(cls.model(**item) for item in
+                          cls.model.collection.find().sort(attribute, sort_in))
