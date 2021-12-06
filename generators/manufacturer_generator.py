e4ace5098bd27dd7ba53ac13eb88d53d04833772
Mahboubeh Azizzadeh <m.a.82@outlook.com>
2021-12-06 14:38:28 +0100

    code for contact_person_id is ready to use

--- generators/manufacturer_generator.py
+++ generators/manufacturer_generator.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 from shared.validators import validate_length
 from app.controllers.manufacturer_controller import ManufacturerController
 from generators.fake_data import FakeData
@@ -8,6 +8,7 @@ class ManufacturerGenerator:
     COMPANY_NAME_MAX_LEN = 45
     HEAD_OFFICE_PHONE_LEN = 25
     HEAD_OFFICE_ADDRESS_LEN = 100
+    cpi = list(range(1, 101))
 
     @staticmethod
     def generate(company_name: str, head_office_phone: str, head_office_address: str) -> None:
@@ -15,11 +16,11 @@ class ManufacturerGenerator:
         validate_length(provided=head_office_phone, limit=ManufacturerGenerator.HEAD_OFFICE_PHONE_LEN)
         validate_length(provided=head_office_address, limit=ManufacturerGenerator.HEAD_OFFICE_ADDRESS_LEN)
 
-        cpi = randint(1, 101)
-        if cpi >= 80:
+        cpi_new = ManufacturerGenerator.cpi.pop()
+        if cpi_new >= 90:
             contact_person_id = None
         else:
-            contact_person_id = cpi
+            contact_person_id = cpi_new
 
         ManufacturerController.create(
             company_name=company_name,
@@ -33,10 +34,13 @@ class ManufacturerGenerator:
         locations = FakeData.generate_locations(amount)
         company_names = FakeData.generate_companies(amount)
 
+        random.shuffle(ManufacturerGenerator.cpi)
+
         for i in range(amount):
             cls.generate(company_name=company_names[i],
                          head_office_phone=phone_numbers[i],
                          head_office_address=locations[i].__str__())
+
         print("----- Manufacturers generated -----")
 
 
