43878e38e78f3e52dfdf8e42016c2b34b67aa839
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-06 01:15:15 +0100

    move CustomerType to shared package

--- app/data/_mysql/models/customer.py
+++ app/data/_mysql/models/customer.py
@@ -4,11 +4,6 @@ from sqlalchemy.orm import relationship
 from app.data._mysql.models import BaseModel
 
 
-class CustomerType:
-    PRIVATE = "p"
-    CORPORATE = "c"
-
-
 class Customer(BaseModel):
     __tablename__ = "customers"
 
