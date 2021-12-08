cd688d8b4516e9883de2ea6e773f8bc9daff911f
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-08 23:43:39 +0100

    Update contact_person_controller.py

--- app/controllers/contact_person_controller.py
+++ app/controllers/contact_person_controller.py
@@ -1,18 +1,20 @@
+from typing import Optional
+
 from app.controllers import BaseController
 from app.data.models.contact_person import ContactPerson
 from app.data.repositories.contact_person_repository import ContactPersonRepository
-from typing import Optional
 
 
 class ContactPersonController(BaseController):
-    model = ContactPerson
+    repository = ContactPersonRepository
+    required_attributes = {"first_name", "last_name", "phone", "email"}
 
     @staticmethod
     def find_by_id(_id: int) -> Optional[ContactPerson]:
         return ContactPersonRepository.find_by_id(_id)
 
     @classmethod
-    def create(cls, first_name: str, last_name: str, phone: str, email: str = None) -> None:
+    def create(cls, first_name: str, last_name: str, phone: str, email: str) -> None:
         ContactPersonRepository.create(first_name=first_name,
                                        last_name=last_name,
                                        phone=phone,
