87de296dff3b0a6718a14f8baa7edb4bdbc85cc8
Amir Ramic <amir_ramic@msn.com>
2021-12-09 11:16:19 +0100

    added repositories

--- app/data/_mysql/models/order.py
+++ app/data/_mysql/models/order.py
@@ -10,9 +10,9 @@ class Order(BaseModel):
     order_id = Column(Integer, autoincrement=True, primary_key=True)
     ordered_date = Column(TIMESTAMP, nullable=False)
     shipped_date = Column(TIMESTAMP, nullable=False)
-    delivery_date = Column(Date,nullable=True)
+    delivery_date = Column(Date, nullable=True)
     status = Column(String(15), nullable=False)
-    customer_id = Column(Integer, ForeignKey("customers.customer_id"), nullable=True)
+    customer_id = Column(Integer, ForeignKey("customers.customer_id"), nullable=False)
 
     customer = relationship("Customer", back_populates="orders")
     order_detail = relationship("OrderDetail", back_populates="order")
--- app/data/_mysql/repositories/order_detail_repository.py
+++ app/data/_mysql/repositories/order_detail_repository.py
@@ -1,8 +1,8 @@
 from typing import Optional
-
 from app.data._mysql.db import session
 from app.data._mysql.repositories import BaseRepository
 from app.data._mysql.models.order_detail import OrderDetail
+from app.data._mysql.models import Product
 
 
 class OrderDetailRepository(BaseRepository):
@@ -11,3 +11,26 @@ class OrderDetailRepository(BaseRepository):
     @classmethod
     def find_by_id(cls, _id: int) -> Optional[OrderDetail]:
         return session.query(cls.model).filter_by(order_detail_id=_id).first()
+
+    @classmethod
+    def find_by_order(cls, order: str, many: bool = False) -> Optional[OrderDetail, list[OrderDetail]]:
+        if many:
+            return session.query(cls.model).filter_by(order=order)
+        return session.query(cls.model).filter_by(order=order).first
+
+    @classmethod
+    def add_product_to_order_detail(cls, orderdetail: OrderDetail, product: Product) -> None:
+        if cls.has_product(orderdetail) or OrderDetailRepository.has_order(product):
+            return
+
+        orderdetail.product_id = product.product_id
+        product.orderdetail = OrderDetail
+        session.commit()
+
+    @classmethod
+    def has_product(cls, orderdetail: OrderDetail) -> bool:
+        return orderdetail.product_id is not None
+
+    @classmethod
+    def has_order(cls, product: Product) -> bool:
+        return product.order_details is not None
