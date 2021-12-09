c6f3b0b64c40767250b1400989a7bf57aad962a3
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-09 04:41:51 +0100

    modify tests to work for both mongo and mysql

--- tests/controllers/test_store_controller.py
+++ tests/controllers/test_store_controller.py
@@ -5,9 +5,9 @@ from app.settings import Database, Settings
 Settings.TESTING = True
 
 from app.controllers.store_controller import StoreController
+from generators.store_generator import StoreGenerator
 from shared.models.types import StoreType
 from shared.tests.test_printer import TestPrinter
-from tests.store_test_data import store_data, stores_data
 
 
 class StoreControllerTestCase(unittest.TestCase):
@@ -29,21 +29,11 @@ class StoreControllerTestCase(unittest.TestCase):
 
     # endregion Setup & Cleanup
 
-    # region Utility
-
-    @classmethod
-    def create_single_store(cls) -> None:
-        StoreController.create(**store_data)
-
-    @classmethod
-    def create_many_stores(cls) -> None:
-        StoreController.create_many(stores_data)
-
-    # region Utility
+    # region Tests
 
     def test_find_by_id_found(self):
-        self.create_single_store()
-        store_created = StoreController.find_by_city("TestCity")
+        StoreGenerator.populate_database(amount=1)
+        store_created = StoreController.find_all()[0]
 
         if Settings.DATABASE == Database.MONGO:
             store = StoreController.find_by_id(store_created._id)
@@ -60,12 +50,6 @@ class StoreControllerTestCase(unittest.TestCase):
             store = StoreController.find_by_id(9999)
         self.assertIsNone(store)
         TestPrinter.add(self.test_find_by_id_not_found.__name__)
-
-    def test_create_one_store(self):
-        self.create_single_store()
-        store = StoreController.find_by_city("TestCity")
-        self.assertIsNotNone(store)
-        TestPrinter.add(self.test_create_one_store.__name__)
 
     def test_create_physical_store_fail(self):
         with self.assertRaises(ValueError):
@@ -85,25 +69,27 @@ class StoreControllerTestCase(unittest.TestCase):
         TestPrinter.add(self.test_create_online_store_fail.__name__)
 
     def test_create_many_stores(self):
-        self.create_many_stores()
+        StoreGenerator.populate_database(amount=3)
         stores = StoreController.find_all()
         self.assertTrue(len(stores) >= 3)
         TestPrinter.add(self.test_create_many_stores.__name__)
 
     def test_find_by_city_found(self):
-        self.create_single_store()
-        store = StoreController.find_by_city("TestCity")
+        StoreGenerator.populate_database(amount=1)
+        store_created = StoreController.find_all()[0]
+        store = StoreController.find_by_city(store_created.city)
         self.assertIsNotNone(store)
         TestPrinter.add(self.test_find_by_city_found.__name__)
 
     def test_find_by_city_not_found(self):
-        store = StoreController.find_by_city("Gothenburg")
+        store = StoreController.find_by_city("The Moon")
         self.assertIsNone(store)
         TestPrinter.add(self.test_find_by_city_not_found.__name__)
 
     def test_find_by_email_found(self):
-        self.create_single_store()
-        store = StoreController.find_by_email("testcity@store.se")
+        StoreGenerator.populate_database(amount=1)
+        store_created = StoreController.find_all()[0]
+        store = StoreController.find_by_email(store_created.email)
         self.assertIsNotNone(store)
         TestPrinter.add(self.test_find_by_email_found.__name__)
 
@@ -113,7 +99,7 @@ class StoreControllerTestCase(unittest.TestCase):
         TestPrinter.add(self.test_find_by_email_not_found.__name__)
 
     def test_find_by_store_type_found(self):
-        self.create_single_store()
+        StoreGenerator.populate_database(amount=1, online=False)
         store = StoreController.find_by_store_type(StoreType.PHYSICAL)
         self.assertIsNotNone(store)
         TestPrinter.add(self.test_find_by_store_type_found.__name__)
@@ -124,7 +110,7 @@ class StoreControllerTestCase(unittest.TestCase):
         TestPrinter.add(self.test_find_by_store_type_not_found.__name__)
 
     def test_find_all_found(self):
-        self.create_many_stores()
+        StoreGenerator.populate_database(amount=3)
         stores = StoreController.find_all()
         self.assertIsNotNone(stores)
         TestPrinter.add(self.test_find_all_found.__name__)
@@ -136,7 +122,7 @@ class StoreControllerTestCase(unittest.TestCase):
         TestPrinter.add(self.test_find_all_not_found.__name__)
 
     def test_delete_all_found(self):
-        self.create_many_stores()
+        StoreGenerator.populate_database(amount=3)
         count_deleted = StoreController.delete_all()
         self.assertTrue(count_deleted >= 3)
         TestPrinter.add(self.test_delete_all_found.__name__)
@@ -144,8 +130,10 @@ class StoreControllerTestCase(unittest.TestCase):
     def test_delete_all_not_found(self):
         StoreController.delete_all()
         count_deleted = StoreController.delete_all()
-        self.assertFalse(count_deleted >= 3)
+        self.assertFalse(count_deleted > 0)
         TestPrinter.add(self.test_delete_all_not_found.__name__)
+
+    # endregion Tests
 
 
 def main():
