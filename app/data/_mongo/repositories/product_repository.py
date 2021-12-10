711177ad1745564a2ce350a70f04f16358085852
Mahboubeh Azizzadeh <m.a.82@outlook.com>
2021-12-10 09:31:43 +0100

    fixes

---
+++ app/data/_mongo/repositories/product_repository.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from typing import Optional
+
+from app.data._mongo.models.product import Product
+from app.data._mongo.repositories import BaseRepository
+
+
+class ProductRepository(BaseRepository):
+    model = Product
+
+
+
+
