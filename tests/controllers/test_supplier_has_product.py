26bd9a053b0da7dfe2d8f5eab534d3b1d6fb16f5
Mahboubeh Azizzadeh <m.a.82@outlook.com>
2021-12-12 21:39:16 +0100

    fixes

--- tests/controllers/test_supplier_has_product.py
+++ tests/controllers/test_supplier_has_product.py
@@ -1,21 +1,20 @@
 import unittest
-
-from app.settings import Settings
-
-Settings.TESTING = True
-
+# from tests.helpers.dbutil import *
 from app.controllers.product_controller import ProductController
-from app.controllers.supplier_controller import SupplierController
-from generators.contact_person_generator import ContactPersonGenerator
 from generators.product_generator import ProductGenerator
+from generators.contact_person_generator import ContactPersonGenerator
+from app.controllers.supplier_controller import SupplierController
 from generators.supplier_generator import SupplierGenerator
 from shared.tests.test_printer import TestPrinter
+from app.settings import Settings
+Settings.TESTING = True
 
 
 class ManufacturerTestCase(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls) -> None:
+        # create_db()
         ContactPersonGenerator.populate_database(amount=100)
         SupplierGenerator.populate_database(amount=1)
         ProductGenerator.populate_database(amount=2)
@@ -23,6 +22,7 @@ class ManufacturerTestCase(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls) -> None:
+        # delete_db()
         TestPrinter.print_passed_tests()
 
     def test_add_product_to_supplier(self):
@@ -32,12 +32,12 @@ class ManufacturerTestCase(unittest.TestCase):
         SupplierController.add_product_to_supplier(supplier, product)
         SupplierController.add_product_to_supplier(supplier, product_two)
         print('----- The two products are: -----')
-        for supplier_product in supplier.products:
-            print(supplier_product.name)
-            self.assertIsNotNone(supplier_product)
+        for suppliers_product in supplier.products:
+            print(suppliers_product.product.name)
+            self.assertIsNotNone(suppliers_product)
         TestPrinter.add(self.test_add_product_to_supplier.__name__)
 
-    def test_remove_product_from_supplier(self):
+    def test_remove_product_from_manufacturer(self):
         product = ProductController.find_all()[0]
         product_two = ProductController.find_all()[1]
         supplier = SupplierController.find_all()[0]
@@ -46,8 +46,8 @@ class ManufacturerTestCase(unittest.TestCase):
         SupplierController.remove_product_from_supplier(supplier, product)
         SupplierController.remove_product_from_supplier(supplier, product_two)
         for suppliers_product in supplier.products:
-            self.assertIsNone(suppliers_product.name)
-        TestPrinter.add(self.test_remove_product_from_supplier.__name__)
+            self.assertIsNone(suppliers_product)
+        TestPrinter.add(self.test_remove_product_from_manufacturer.__name__)
 
 
 def main():
