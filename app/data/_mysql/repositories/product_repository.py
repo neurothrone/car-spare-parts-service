22f6368bfbf7a33f33e3c05fa8815833a1b0ecaf
Amir Ramic <amir_ramic@msn.com>
2021-12-13 14:18:53 +0100

    worked on product repository

--- app/data/_mysql/repositories/product_repository.py
+++ app/data/_mysql/repositories/product_repository.py
@@ -1,5 +1,5 @@
 from typing import Optional, Union
-
+from app.data._mysql.models.car_detail import CarDetail
 from app.data._mysql.db import session
 from app.data._mysql.repositories import BaseRepository
 from app.data._mysql.models.product import Product
@@ -35,3 +35,31 @@ class ProductRepository(BaseRepository):
         if many:
             return session.query(cls.model).filter_by(price=price)
         return session.query(cls.model).filter_by(price=price).first()
+
+    @classmethod
+    def add_product_to_car_detail(cls, product: Product,
+                                  car_detail: CarDetail) -> None:
+        if cls.has_product(product, car_detail):
+            return
+
+        product.car_details.append(car_detail)
+        session.commit()
+
+    @classmethod
+    def remove_car_detail_from_product(cls, product: Product,
+                                       car_detail: CarDetail) -> None:
+        if not cls.has_product(product, car_detail):
+            return
+
+        product.car_details.remove(car_detail)
+        session.commit()
+
+    @classmethod
+    def has_product(cls, product: Product, car_detail: CarDetail) -> bool:
+        if not product.car_details:
+            return False
+
+        for product_car_detail in product.car_details:
+            if product_car_detail.car_detail_id == car_detail.car_detail_id:
+                return True
+        return False
