8deead967a6d4697dcbdad1b29884c2de69b19e6
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-15 06:10:37 +0100

    add automation feature to populate and delete data from mysql database

--- app/views/web/routes.py
+++ app/views/web/routes.py
@@ -2,12 +2,13 @@ import flask
 
 from app.settings import Settings
 
-Settings.TESTING = True
+Settings.TESTING = False
 
 from app.controllers.customer_controller import CustomerController
 from app.controllers.employee_controller import EmployeeController
 from app.controllers.product_controller import ProductController
 from app.controllers.store_controller import StoreController
+import generators.db_control as db_control
 
 bp = flask.Blueprint("main", __name__, template_folder="templates")
 
@@ -29,7 +30,7 @@ def stores_page():
     return flask.render_template("items/store/content.html", stores=stores)
 
 
-@bp.post("/stores/create")
+@bp.route("/stores/create", methods=["POST"])
 def create_store():
     data = {key: (value if value else None) for key, value in flask.request.form.items()}
     try:
@@ -51,3 +52,47 @@ def employees_page():
 def customers_page():
     customers = CustomerController.find_all()
     return flask.render_template("items/customer/content.html", customers=customers)
+
+
+@bp.route("/db/populate", methods=["POST"])
+def populate():
+    try:
+        db_control.populate_mysql_db()
+        flask.flash("MySQL database successfully populated", "success")
+    except Exception as error:
+        print(error)
+        flask.flash("Something went wrong when populating MySQL database", "error")
+    return flask.redirect(flask.url_for("main.index"))
+
+
+@bp.route("/db/convert", methods=["POST"])
+def convert():
+    try:
+        db_control.convert_mysql_to_mongo()
+        flask.flash("Data in MySQL database successfully converted to data in Mongo database", "success")
+    except Exception as error:
+        print(error)
+        flask.flash("Something went wrong with converting from MySQL to Mongo", "error")
+    return flask.redirect(flask.url_for("main.index"))
+
+
+@bp.route("/db/purge-mysql", methods=["POST"])
+def purge_mysql():
+    try:
+        db_control.delete_data_from_mysql_db()
+        flask.flash("MySQL database successfully purged", "success")
+    except Exception as error:
+        print(error)
+        flask.flash("Something went wrong when purging MySQL database", "error")
+    return flask.redirect(flask.url_for("main.index"))
+
+
+@bp.route("/db/purge-mongo", methods=["POST"])
+def purge_mongo():
+    try:
+        db_control.delete_data_from_mongo_db()
+        flask.flash("Mongo database successfully purged", "success")
+    except Exception as error:
+        print(error)
+        flask.flash("Something went wrong when purging Mongo database", "error")
+    return flask.redirect(flask.url_for("main.index"))
--- generators/db_control.py
+++ generators/db_control.py
@@ -10,13 +10,17 @@ from generators.storage_generator import StorageGenerator
 from generators.store_generator import StoreGenerator
 
 
-def populate_db():
+def populate_mysql_db():
     StoreGenerator.populate_database(amount=10)
     EmployeeGenerator.populate_database(amount=30)
     EmployeeController.connect_employees_to_stores(min_=1, max_=3)
     CustomerGenerator.populate_database(amount=30)
     ProductGenerator.populate_database(amount=50)
     StorageGenerator.add_products_to_stores(min_per_store=1, max_per_store=5)
+
+
+def convert_mysql_to_mongo():
+    pass
 
 
 def delete_data_from_mysql_db():
@@ -27,17 +31,5 @@ def delete_data_from_mysql_db():
     StoreController.delete_all()
 
 
-def main():
-    # populate_db()
-    delete_data_from_mysql_db()
-    # EmployeeController.connect_employees_to_stores(min_=1, max_=3)
-    # EmployeeController.reset_all_employees_store()
-
-    # StoreGenerator.populate_database(amount=100)
-    # StoreGenerator.add_products_to_stores(min_per_store=1, max_per_store=5)
-    # StoreGenerator.print_products_in_stores()
-    # StoreGenerator.remove_all_products_in_stores()
-
-
-if __name__ == "__main__":
-    main()
+def delete_data_from_mongo_db():
+    pass
