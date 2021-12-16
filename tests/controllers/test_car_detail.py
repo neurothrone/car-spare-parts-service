bd70f302200bc0c9e739e1a247137a432732b40e
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-16 07:50:20 +0100

    fix tests and rename variables to improve readability

--- tests/controllers/test_car_detail.py
+++ tests/controllers/test_car_detail.py
@@ -1,13 +1,15 @@
+import unittest
+
+from app.settings import Database, Settings
+
+Settings.TESTING = True
+
 from app.controllers.car_detail_controller import CarDetailController
 from generators.car_detail_generator import CarDetailGenerator
 from shared.tests.test_printer import TestPrinter
-import unittest
-from app.settings import Database, Settings
-Settings.TESTING = True
 
 
 class CarDetailControllerTestCase(unittest.TestCase):
-
     def setUp(self) -> None:
         CarDetailController.delete_all()
 
@@ -23,7 +25,6 @@ class CarDetailControllerTestCase(unittest.TestCase):
         TestPrinter.print_passed_tests()
 
     def test_find_by_id_found(self):
-
         CarDetailGenerator.populate_database(amount=1)
         car_detail_created = CarDetailController.find_all()[0]
 
@@ -37,50 +38,54 @@ class CarDetailControllerTestCase(unittest.TestCase):
 
     def test_find_by_id_not_found(self):
         if Settings.DATABASE == Database.MONGO:
-            store = CarDetailController.find_by_id("61acd805a1dca9c019999999")
+            car_detail = CarDetailController.find_by_id("61acd805a1dca9c019999999")
         else:
-            store = CarDetailController.find_by_id(9999)
-        self.assertIsNone(store)
+            car_detail = CarDetailController.find_by_id(0)
+
+        self.assertIsNone(car_detail)
         TestPrinter.add(self.test_find_by_id_not_found.__name__)
 
     def test_find_by_brand_found(self):
         CarDetailGenerator.populate_database(amount=1)
         car_detail_created = CarDetailController.find_all()[0]
         car_detail = CarDetailController.find_by_brand(car_detail_created.brand)
+
         self.assertIsNotNone(car_detail)
         TestPrinter.add(self.test_find_by_brand_found.__name__)
 
     def test_find_by_brand_not_found(self):
-        brand = CarDetailController.find_by_brand("The Laser gun")
-        self.assertIsNone(brand)
+        car_detail = CarDetailController.find_by_brand("The Laser gun")
+        self.assertIsNone(car_detail)
         TestPrinter.add(self.test_find_by_brand_not_found.__name__)
 
     def test_find_by_model_found(self):
         CarDetailGenerator.populate_database(amount=1)
         car_detail_created = CarDetailController.find_all()[0]
-        model = CarDetailController.find_by_model(car_detail_created.model)
-        self.assertIsNotNone(model)
+        car_detail = CarDetailController.find_by_model(car_detail_created.model)
+        self.assertIsNotNone(car_detail)
         TestPrinter.add(self.test_find_by_brand_found.__name__)
 
     def test_find_by_model_not_found(self):
-        model = CarDetailController.find_by_model("Extra bacon")
-        self.assertIsNone(model)
+        car_detail = CarDetailController.find_by_model("Extra bacon")
+        self.assertIsNone(car_detail)
         TestPrinter.add(self.test_find_by_model_not_found.__name__)
 
     def test_find_by_year_found(self):
         CarDetailGenerator.populate_database(amount=1)
         car_detail_created = CarDetailController.find_all()[0]
-        year = CarDetailController.find_by_year(car_detail_created.year)
-        self.assertIsNotNone(year)
+        car_detail = CarDetailController.find_by_year(car_detail_created.year)
+        self.assertIsNotNone(car_detail)
         TestPrinter.add(self.test_find_by_brand_found.__name__)
 
     def test_find_by_year_not_found(self):
-        year = CarDetailController.find_by_year("Around 9000")
-        self.assertIsNone(year)
+        car_detail = CarDetailController.find_by_year("Around 9000")
+        self.assertIsNone(car_detail)
         TestPrinter.add(self.test_find_by_year_not_found.__name__)
 
 
 def main():
+    unittest.main()
 
-    if __name__ == '__main__':
-        main()
+
+if __name__ == "__main__":
+    main()
