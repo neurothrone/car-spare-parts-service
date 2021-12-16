639ac4b53d5a477d44ea28f690c27077687d93e9
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-16 08:41:57 +0100

    fix imports and set temporary chain of statements to see details

--- app/converters/car_converter.py
+++ app/converters/car_converter.py
@@ -1,21 +1,31 @@
-from app.data._mongo.repositories.car_repository import CarRepository as MongoProductRepository
-from app.data._mysql.repositories.car_repository import CarRepository as MysqlProductRepository
-
+import pprint
 
 from app.settings import Settings
+from generators.car_generator import CarGenerator
 
 Settings.TESTING = True
+
+from app.data._mongo.repositories.car_repository import CarRepository as MongoCarRepository
+from app.data._mysql.repositories.car_repository import CarRepository as MysqlCarRepository
 
 
 class CarConverter:
     @classmethod
     def convert_from_mysql_to_mongo(cls):
-        for car in MysqlProductRepository.find_all():
+        for car in MysqlCarRepository.find_all():
             as_dict = car.__dict__
             del as_dict["_sa_instance_state"]
-            MongoProductRepository.create(**as_dict)
+
+            pprint.pprint(as_dict)
+            quit()
+
+            MongoCarRepository.create(**as_dict)
 
 
 def main():
-    if __name__ == '__main__':
-        main()
+    # CarGenerator.generate_cars(amount=10)
+    CarConverter.convert_from_mysql_to_mongo()
+
+
+if __name__ == "__main__":
+    main()
