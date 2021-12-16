08635be7a774d4b1adafccbbf28b3da510edea7d
Mahboubeh Azizzadeh <m.a.82@outlook.com>
2021-12-16 14:58:01 +0100

    take over Amirs table "order" to create mongo order_repository.py under mongo is created

--- app/data/_mongo/repositories/order_repository.py
+++ app/data/_mongo/repositories/order_repository.py
@@ -1,6 +1,43 @@
 from __future__ import annotations
+from datetime import datetime
+from typing import Optional
+
+from app.data._mongo.models.order import Order
+from app.data._mongo.models.product import Product
 from app.data._mongo.repositories import BaseRepository
 
 
 class OrderRepository(BaseRepository):
-    pass
+    model = Order
+
+    @classmethod
+    def find_by_ordered_date(cls, ordered_date: datetime,
+                             many: bool = False) -> Optional[Order | list[Order]]:
+        return cls.find(many=many, ordered_date=ordered_date)
+
+    @classmethod
+    def find_by_shipped_date(cls, shipped_date: datetime,
+                             many: bool = False) -> Optional[Order | list[Order]]:
+        return cls.find(many=many, shipped_date=shipped_date)
+
+    @classmethod
+    def find_by_delivery_date(cls, delivery_date: datetime.date,
+                              many: bool = False) -> Optional[Order | list[Order]]:
+        return cls.find(many=many, delivery_date=delivery_date)
+
+    @classmethod
+    def find_by_status(cls, status: str, many: bool = False) -> Optional[Order | list[Order]]:
+        return cls.find(many=many, status=status)
+
+    @classmethod
+    def add_order_to_product(cls, order: Order, product: Product) -> None:
+        pass
+
+    @classmethod
+    def remove_order_from_product(cls, order: Order, product: Product) -> None:
+        pass
+
+    @classmethod
+    def has_product(cls, order: Order, product: Product) -> bool:
+        pass
+
