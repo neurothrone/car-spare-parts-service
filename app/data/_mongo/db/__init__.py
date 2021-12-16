837d67c4573d16f186dabf1c2c1a06e22edae26a
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-16 11:47:52 +0100

    add method for deleting mongo database

--- app/data/_mongo/db/__init__.py
+++ app/data/_mongo/db/__init__.py
@@ -9,3 +9,11 @@ if Settings.TESTING:
     db = client[f"{MongoConfig.DB_NAME}-test"]
 else:
     db = client[MongoConfig.DB_NAME]
+
+
+def drop_mongo_db(db_name: str):
+    if db_name in client.list_database_names():
+        client.drop_database(db_name)
+        print(f"DB deleted: {db_name}")
+    else:
+        print(f"DB does not exist: {db_name}")
