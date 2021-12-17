d00e56f5960ce6cdfbcfccd22a4dcad0132c0abd
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-17 04:03:28 +0100

    fix order detail converter (still lacking)

--- app/converters/order_detail_converter.py
+++ app/converters/order_detail_converter.py
@@ -1,3 +1,5 @@
+import pprint
+
 from app.settings import Settings
 
 Settings.TESTING = True
@@ -5,33 +7,33 @@ Settings.TESTING = True
 from app.data._mongo.repositories.order_detail_repository import OrderDetailRepository as MongoOrderDetailRepository
 from app.data._mysql.repositories.order_detail_repository import OrderDetailRepository as MysqlOrderDetailRepository
 
-from controllers.order_detail_controller import OrderDetailController
-# from data._mongo.repositories.order_repository import OrderRepository as MongoOrderRepository
-
 
 class OrderDetailConverter:
     @classmethod
     def convert_from_mysql_to_mongo(cls):
         for order_detail in MysqlOrderDetailRepository.find_all():
             as_dict = order_detail.__dict__
-            as_dict = {key: str(value) for key, value in as_dict.items() if value is not None}
-            # as_dict['contact_person_id'] = MongoOrderRepository.find(contact_person_id=manufacturer.contact_person_id)._id
+            as_dict = {key: value for key, value in as_dict.items() if value is not None}
 
-            order_detail = OrderDetailController.find_by_id(order_detail.order_detail_id)
             as_dict["quantity_ordered"] = order_detail.quantity_ordered
-            as_dict["price_each"] = order_detail.price_each
+            as_dict["price_each"] = float(order_detail.product.price)
 
+            as_dict["product"] = {
+                "cost": float(order_detail.product.cost),
+                "price": float(order_detail.product.price),
+                "name": order_detail.product.name,
+                "product_id": order_detail.product.product_id
+            }
+
+            del as_dict["product_id"]
             del as_dict["_sa_instance_state"]
 
-            products = []
-            for product in order_detail.products:
-                products.append({
-                    "cost": float(product.cost),
-                    "price": float(product.price),
-                    "name": product.name,
-                    "product_id": product.product_id
-                })
-
-            as_dict['products'] = products
-
             MongoOrderDetailRepository.create(**as_dict)
+
+
+def main():
+    OrderDetailConverter.convert_from_mysql_to_mongo()
+
+
+if __name__ == "__main__":
+    main()
