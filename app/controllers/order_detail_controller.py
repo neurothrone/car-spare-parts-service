25296157840127c8e99f80bbf8e83951d06caed0
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-17 04:02:46 +0100

    remove unused import

--- app/controllers/order_detail_controller.py
+++ app/controllers/order_detail_controller.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 from typing import Optional
-from unicodedata import decimal
 
 from app.controllers import BaseController
 from app.data.models.order_detail import OrderDetail
