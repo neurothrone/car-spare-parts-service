c386797d0505dccd278cb8259a6101226eeab92a
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2021-12-17 04:45:07 +0100

    fix order generator

--- generators/order_generator.py
+++ generators/order_generator.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
-from app.settings import Settings, Database
+import datetime
+from random import choice
+
+from app.settings import Settings
 
 Settings.TESTING = True
 
 from app.controllers.customer_controller import CustomerController
 from app.controllers.order_controller import OrderController
 from generators.customer_generator import CustomerGenerator
+from generators.fake_data import FakeData
 from shared.validators import validate_length
-import random
-import datetime
-from faker import Faker
 
 
 class OrderGenerator:
@@ -17,7 +18,7 @@ class OrderGenerator:
 
     @classmethod
     def generate(cls, ordered_date: datetime, shipped_date: datetime,
-                 delivery_date: datetime, status: str, customer_id: int) -> None:
+                 delivery_date: datetime.date, status: str, customer_id: int) -> None:
         validate_length(provided=status, limit=OrderGenerator.STATUS_LEN)
 
         OrderController.create(ordered_date=ordered_date, shipped_date=shipped_date,
@@ -27,33 +28,37 @@ class OrderGenerator:
     def populate_database(cls, amount: int) -> None:
         CustomerGenerator.populate_database(amount)
 
-        fake = Faker()
-        start_date_ordered_dates = datetime.date(year=2021, month=1, day=1)
-        end_date_ordered_dates = datetime.date(year=2021, month=1, day=15)
+        ordered_datetimes = FakeData.generate_datetimes(
+            amount,
+            start=datetime.date(year=2021, month=1, day=1),
+            end=datetime.date(year=2021, month=1, day=15))
+        shipped_datetimes = FakeData.generate_datetimes(
+            amount,
+            start=datetime.date(year=2021, month=1, day=15),
+            end=datetime.date(year=2021, month=1, day=30))
+        delivery_datetimes = FakeData.generate_datetimes(
+            amount,
+            start=datetime.date(year=2021, month=1, day=30),
+            end=datetime.date(year=2021, month=3, day=30))
+        customers = CustomerController.find_all()
 
-        start_date_shipped_dates = datetime.date(year=2021, month=1, day=15)
-        end_date_shipped_dates = datetime.date(year=2021, month=1, day=30)
+        for ordered_datetime, shipped_datetime, delivery_date, customer in zip(
+                ordered_datetimes, shipped_datetimes, delivery_datetimes, customers):
 
-        start_date_delivery_dates = datetime.date(year=2021, month=1, day=30)
-        end_date_delivery_dates = datetime.date(year=2021, month=3, day=30)
-
-        for i in range(amount):
-            if Settings.DATABASE == Database.MONGO:
-                pass
-
+            if not shipped_datetime:
+                status = "Processing"
+            elif delivery_date.date() < datetime.datetime.today().date():
+                status = "Delivered"
+            elif shipped_datetime < delivery_date:
+                status = "On delivery"
             else:
-                customer_id = random.choice(CustomerController.find_all()).customer_id
+                status = "To be updated..."
 
-                ordered_dates = fake.date_between(start_date=start_date_ordered_dates, end_date=end_date_ordered_dates)
-                shipped_dates = fake.date_between(start_date=start_date_shipped_dates, end_date=end_date_shipped_dates)
-                delivery_dates = fake.date_between(start_date=start_date_delivery_dates,
-                                                   end_date=end_date_delivery_dates)
-
-                cls.generate(ordered_date=ordered_dates,
-                             shipped_date=shipped_dates,
-                             delivery_date=delivery_dates,
-                             status="Processing",
-                             customer_id=customer_id)
+            cls.generate(ordered_date=ordered_datetime,
+                         shipped_date=shipped_datetime,
+                         delivery_date=delivery_date.date(),
+                         status=status,
+                         customer_id=choice(customers).customer_id)
 
         print(f"----- {amount} orders generated -----")
 
